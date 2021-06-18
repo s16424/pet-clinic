2717e1e94b885d3948b073d8f55c089ae0f277e5
AnnaPodolska <64683879+AnnaPodolska@users.noreply.github.com>
2021-06-18 12:51:10 +0200

    adding base entity

--- pet-clicic-data/src/main/java/ania/spring/petclinic/model/Person.java
+++ pet-clicic-data/src/main/java/ania/spring/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ania.spring.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clicic-data/src/main/java/ania/spring/petclinic/services/VetService.java
+++ pet-clicic-data/src/main/java/ania/spring/petclinic/services/VetService.java
@@ -5,6 +5,7 @@ import ania.spring.petclinic.model.Vet;
 import java.util.Set;
 
 public interface VetService {
+
     Vet findByLastName(String lastName);
 
     Vet findById(Long id);
