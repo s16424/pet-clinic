f52237af6f9741ed51dc13cb190bd2fd428d3853
AnnaPodolska <64683879+AnnaPodolska@users.noreply.github.com>
2021-06-21 14:17:53 +0200

    services generics

--- pet-clicic-data/src/main/java/ania/spring/petclinic/services/VetService.java
+++ pet-clicic-data/src/main/java/ania/spring/petclinic/services/VetService.java
@@ -2,15 +2,8 @@ package ania.spring.petclinic.services;
 
 import ania.spring.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
     Vet findByLastName(String lastName);
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
